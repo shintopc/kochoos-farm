7f920c07b802e68ab26cfcdf5e975dd0416d87c8
shintopc <50198933+shintopc@users.noreply.github.com>
2025-04-01 22:24:07 +0530

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,3 +1,9 @@
+// supabase.js
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+export const supabase = createClient(supabaseUrl, supabaseKey);
 document.addEventListener('DOMContentLoaded', async () => {
     // Tab functionality
     const tabs = document.querySelectorAll('.tab-btn');

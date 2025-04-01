1887a2938ab4eb8949fe4fdf2723a61a5c13ab6e
shintopc <50198933+shintopc@users.noreply.github.com>
2025-04-01 22:24:27 +0530

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,4 +1,9 @@
+// supabase.js
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+export const supabase = createClient(supabaseUrl, supabaseKey);
 document.addEventListener('DOMContentLoaded', () => {
     const loginForm = document.getElementById('loginForm');
     

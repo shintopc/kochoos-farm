d7c76bc4a4d415fa4441f72f20d9296f385bef57
shintopc <50198933+shintopc@users.noreply.github.com>
2025-04-01 22:05:28 +0530

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,5 +1,6 @@
-// Initialize Supabase
-import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://txiukritnbhyazeopisa.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR4aXVrcml0bmJoeWF6ZW9waXNhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMzOTIzNjMsImV4cCI6MjA1ODk2ODM2M30.ssAJqsaHPGd03qHPZFok_Toa-WrhpHffW5-vCOWveTk';
+// js/supabase.js
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = supabase.createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
